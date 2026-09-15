18dd2b1ccc16d42ea86bf4a1ab9c3e0eeddacc44
mh <parkmyounghoi@dgsw.hs.kr>
2026-09-15 15:28:19 +0900

    no message

--- ex06/src/main/java/com/example/ex06/service/ProductService.java
+++ ex06/src/main/java/com/example/ex06/service/ProductService.java
@@ -1,8 +1,13 @@
 package com.example.ex06.service;
 
+import com.example.ex06.entity.Member;
+import com.example.ex06.entity.Product;
 import com.example.ex06.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -13,5 +18,14 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public List<Product> findAll() {
+        // select * from member;
+        return productRepository.findAll();
+    }
 
+    public Product findById(Long id) {
+        // select * from member where id = ?id;
+        Optional<Product> optionalProduct =  productRepository.findById(id);
+        return optionalProduct.get();
+    }
 }
